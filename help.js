43640bb2aacf6f7f2a24c4a75e262e6dc5b08119
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2023-07-15 11:16:33 -0500

    sintax error fixed

--- help.js
+++ help.js
@@ -1,11 +1,11 @@
-funtion sumar(num1, nu2) {
-  return num1+num2;
+function sumar(num1, num2) {
+  return num1 + num2;
 }
 
-funtion restar(num1, num2) {
+function restar(num1, num2) {
   return num1 - num2;
 }
 
-funtion multiplicar(num1, num2) {
+function multiplicar(num1, num2) {
   return num1 * num2;
 }
